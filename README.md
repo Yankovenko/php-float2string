7d7884b759e751c4026c61bd21d0047d3b22842e
yankovenko <r.yankovenko@platform.live>
2024-01-04 17:56:01 +0100

    +

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# php-float2string
+# Float 2 string
+
+It was surprising that this feature was not standard. 
+And in general, I haven’t found anything similar on the Internet, although everything seems simple.
+
+The main problem solved by the function is the rejection of short recording,
+such as `1.234E-10`, without conversion.
+
+So, use it without restrictions.
+
---
+++ float2string.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$tests = [
+    ['float' => 1.2345678901234E+20, 'string' => "123456789012340000000"], // 20-13 = 7 => add right
+    ['float' => 1.2345678901234E-20, 'string' => "0.000000000000000000012345678901234"], // -20-13 = -33 => add left 0.0..., length = 35
+    ['float' => 1234567890123456789, 'string' => "1234567890123456789"],
+    ['float' => 999999999999999999, 'string' => "999999999999999999"],
+    ['float' => 0.99999999999999, 'string' => "0.99999999999999"],
+    ['float' => 99999999999999E-13, 'string' => "9.9999999999999"],
+    ['float' => 99999999999999E+13, 'string' => "999999999999990000000000000"], // 13 => add right 0
+    ['float' => 9.9999999999999E+13, 'string' => "99999999999999"], // 13-13 = 0 =>
+];
+
+var_dump($tests);
+$i = 0;
+foreach ($tests as $test) {
+    $float = $test['float'];
+    $string = $test['string'];
+    echo 'test: ' . (++$i) . PHP_EOL;
+    $r = float2string($float);
+    echo $string . ' - ' . $r . ' => ' . ($r === $string ? 'OK' : 'FAIL') . PHP_EOL;
+}
+
+function float2string($float): string
+{
+    $strFloat = (string)$float;
+    if (stripos($strFloat, 'e') && preg_match('/^(\d+)(\.(\d+))?E(\+|-)?(\d+)$/i', $strFloat, $match)) {
+        $dotPosition = ($match[4] === '-' ? -1 : +1) * (int)$match[5] - strlen($match[3]);
+        $strFloat = $match[1] . $match[3];
+        if ($dotPosition > 0) {
+            $strFloat .= str_repeat('0', $dotPosition);
+        } elseif (strlen($strFloat) + $dotPosition < 0) {
+            $strFloat = '0.' . str_repeat('0', -strlen($strFloat) - $dotPosition) . $strFloat;
+        } else {
+            $strFloat = substr($strFloat, 0, $dotPosition) . 'tests' . substr($strFloat, $dotPosition);
+        }
+    }
+    return $strFloat;
+}
